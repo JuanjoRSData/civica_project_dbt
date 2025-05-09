07415e038faacb6fa5948697f191af7bd0305a7c
Juanjo Rubio <DataEngineerJJRS@gmail.com>
2025-05-09 17:47:26 +0000

    version 01 de la capa silver al completo, pendiente de revision

--- models/staging/sql_server_dbo/sql_server_dbo/stg_inventories.sql
+++ models/staging/sql_server_dbo/sql_server_dbo/stg_inventories.sql
@@ -4,20 +4,17 @@
     )
 }}
 
-WITH src_products AS (
+WITH src_inventory AS (
     SELECT 
         *,
-        CURRENT_DATE() AS actual_date 
+        CURRENT_DATE() AS actual_date
     FROM {{ source('sql_server_dbo', 'products')}}
 ),
 renamed_casted AS (
     SELECT 
-      {{ dbt_utils.generate_surrogate_key(['product_id'])}} AS product_id,
-      price,
-      name,
       {{ dbt_utils.generate_surrogate_key(['product_id', 'actual_date'])}} AS inventory_id,
-      _fivetran_deleted,
-      CONVERT_TIMEZONE('UTC', CAST(_fivetran_synced AS timestamp_tz)) AS date_load
+      inventory AS product_stock,
+      actual_date AS date_id,
     FROM  src_products  
 )
 SELECT * FROM renamed_casted
